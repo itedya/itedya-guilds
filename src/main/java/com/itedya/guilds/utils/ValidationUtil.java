64c65fac8c55361fe90bffff218ebbbf0a60c120
itedya <kontakt@itedya.com>
2022-02-20 23:38:28 +0100

    Create validation utility for new validation design
    Created validation utility that has static methods. It can return some variable.
    
    If argument does not pass validation, ValidationException is thrown.

---
+++ src/main/java/com/itedya/guilds/utils/ValidationUtil.java
@@ -0,0 +1,66 @@
+package com.itedya.guilds.utils;
+
+import com.itedya.guilds.daos.MemberDao;
+import com.itedya.guilds.daos.WorldGuardDao;
+import com.itedya.guilds.exceptions.ValidationException;
+import com.itedya.guilds.models.Member;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Objects;
+
+public class ValidationUtil {
+    public static Player senderIsPlayer(CommandSender sender) throws ValidationException {
+        if (!(sender instanceof Player player)) {
+            throw new ValidationException("Musisz byc w grze zeby uzywac tej komendy!");
+        }
+
+        return player;
+    }
+
+    public static void senderHasPermission(CommandSender sender, String permission) throws ValidationException {
+        if (!(sender.hasPermission(permission))) {
+            throw new ValidationException("Brak permisji");
+        }
+    }
+
+    public static void playerIsNotInGuild(Player player) throws ValidationException {
+        MemberDao memberDao = MemberDao.getInstance();
+
+        Member member = memberDao.getByPlayerUuid(player.getUniqueId().toString());
+
+        if (member != null) {
+            throw new ValidationException("Jestes juz w gildii!");
+        }
+    }
+
+    public static World playerIsInWorld(Player player, World world) throws ValidationException {
+        World playersWorld = player.getWorld();
+
+        if (!world.getName().equals(playersWorld.getName())) {
+            throw new ValidationException("Nie jestes w dobrym swiecie!");
+        }
+
+        return world;
+    }
+
+    public static void thereIsEnoughPlaceForCuboid(Location location) throws ValidationException {
+        WorldGuardDao worldGuardDao = new WorldGuardDao(BukkitAdapter.adapt(Objects.requireNonNull(Bukkit.getWorld("world"))));
+
+        ProtectedCuboidRegion region = new ProtectedCuboidRegion(
+                "itedya_guilds_cuboid_test",
+                BlockVector3.at(location.getX() + 50, -500, location.getZ() + 50),
+                BlockVector3.at(location.getX() - 50, 500, location.getZ() - 50)
+        );
+
+        if (worldGuardDao.doesCuboidIntersect(region)) {
+            throw new ValidationException("Jakis cuboid juz istnieje na tym terenie!");
+        }
+    }
+}
